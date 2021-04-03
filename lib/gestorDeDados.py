e6c707e105dc27e04d226aa4fbf12751ba2c412d
Antônio Carlos <jhunny.tec@gmaill.com>
2021-04-03 00:55:38 -0300

    Novas funções referentes ao controle dos dados.

--- lib/gestorDeDados.py
+++ lib/gestorDeDados.py
@@ -10,21 +10,20 @@ def geraDados(numeroDeProdutos):
     # cria o objeto de gravação
     w = csv.writer(file)
 
-    w.writerow(['produto', '1', '2', '3', '4', '5', '6', 'total', 'relativo', 'acumulado', 'classificacao', 'mediaDia',
+    w.writerow(['produto', '1', '2', '3', '4', '5', '6', 'total', 'classificacao', 'mediaDia',
                 'estoqueIdeal'])
+
+    numeroDeProdutosMax = 20
 
     for i in range(numeroDeProdutos):
         id = i+1
-        mes_1 = numAleatorio(0, 10)
-        mes_2 = numAleatorio(0, 10)
-        mes_3 = numAleatorio(0, 10)
-        mes_4 = numAleatorio(0, 10)
-        mes_5 = numAleatorio(0, 10)
-        mes_6 = numAleatorio(0, 10)
+        mes_1 = numAleatorio(0, numeroDeProdutosMax)
+        mes_2 = numAleatorio(0, numeroDeProdutosMax)
+        mes_3 = numAleatorio(0, numeroDeProdutosMax)
+        mes_4 = numAleatorio(0, numeroDeProdutosMax)
+        mes_5 = numAleatorio(0, numeroDeProdutosMax)
+        mes_6 = numAleatorio(0, numeroDeProdutosMax)
         total = (mes_1 + mes_2 + mes_3 + mes_4 +mes_5 + mes_6)
-
-
-
         numParaClassif = random.random()
 
         if (numParaClassif < 0.2):
@@ -34,8 +33,18 @@ def geraDados(numeroDeProdutos):
         else:
             classificacao = 'C'
 
-        w.writerow([id, mes_1, mes_2, mes_3, mes_4, mes_5, mes_6, total, classificacao])
+        mediaDianum = total/180
+        mediaDia = ("{0:.2f}" .format(mediaDianum))
+
+        if (classificacao == 'A'):
+            estoqueIdeal = mediaDianum * 30
+        elif (classificacao == 'B'):
+            estoqueIdeal = mediaDianum * 20
+        else:
+            estoqueIdeal = mediaDianum * 10
+        estoqueIdeal = ("{0:.2f}".format(estoqueIdeal))
+
+        w.writerow([id, mes_1, mes_2, mes_3, mes_4, mes_5, mes_6, total, classificacao, mediaDia, estoqueIdeal])
     file.close()
 
-def calculaDados(file):
-    pass
+#geraDados(10)
