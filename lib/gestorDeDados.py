b58c24f2f777d6360216717cb955127cb7bbb9fb
Antônio Carlos <jhunny.tec@gmaill.com>
2021-04-09 23:50:51 -0300

    Corrigindo alguns erros referentes a graficos e funções.

--- lib/gestorDeDados.py
+++ lib/gestorDeDados.py
@@ -1,18 +1,18 @@
 import csv
 import random
 from matplotlib import pyplot as plt
-import tkinter as tk
-from tkinter import ttk
 
 NUMERODEMESES = 6
-numeroDeProdutos = 10
+NUMERODEPRODUTOS = 10
 
-#Gerar um unmero aleatorio entre a e b.
-def numAleatorio(a, b):
+
+# Gerar um numero aleatorio entre a e b.
+def num_aleatorio(a, b):
     return random.randint(a, b)
 
-#Geraar uma planilha de dados de estoque
-def geraDados(numeroDeProdutos):
+
+# Gerar uma planilha de dados de estoque
+def gera_dados(numerodeprodutos):
     file = open('../src/estoque.csv', 'w', newline='', encoding='utf-8')
 
     # cria o objeto de gravação
@@ -23,30 +23,30 @@ def geraDados(numeroDeProdutos):
 
     numeroDeProdutosMax = 40
 
-    for i in range(numeroDeProdutos):
-        id = i+1
-        mes_1 = numAleatorio(0, numeroDeProdutosMax)
-        mes_2 = numAleatorio(0, numeroDeProdutosMax)
-        mes_3 = numAleatorio(0, numeroDeProdutosMax)
-        mes_4 = numAleatorio(0, numeroDeProdutosMax)
-        mes_5 = numAleatorio(0, numeroDeProdutosMax)
-        mes_6 = numAleatorio(0, numeroDeProdutosMax)
-        total = (mes_1 + mes_2 + mes_3 + mes_4 +mes_5 + mes_6)
+    for i in range(numerodeprodutos):
+        id = i + 1
+        mes_1 = num_aleatorio(0, numeroDeProdutosMax)
+        mes_2 = num_aleatorio(0, numeroDeProdutosMax)
+        mes_3 = num_aleatorio(0, numeroDeProdutosMax)
+        mes_4 = num_aleatorio(0, numeroDeProdutosMax)
+        mes_5 = num_aleatorio(0, numeroDeProdutosMax)
+        mes_6 = num_aleatorio(0, numeroDeProdutosMax)
+        total = (mes_1 + mes_2 + mes_3 + mes_4 + mes_5 + mes_6)
         numParaClassif = random.random()
 
-        if (numParaClassif < 0.2):
+        if numParaClassif < 0.25:
             classificacao = 'A'
-        elif (numParaClassif < 0.5):
+        elif numParaClassif < 0.6:
             classificacao = 'B'
         else:
             classificacao = 'C'
 
-        mediaDianum = total/180
-        mediaDia = ("{0:.2f}" .format(mediaDianum))
+        mediaDianum = total / 180
+        mediaDia = ("{0:.2f}".format(mediaDianum))
 
-        if (classificacao == 'A'):
+        if classificacao == 'A':
             estoqueIdeal = mediaDianum * 30
-        elif (classificacao == 'B'):
+        elif classificacao == 'B':
             estoqueIdeal = mediaDianum * 20
         else:
             estoqueIdeal = mediaDianum * 10
@@ -54,10 +54,10 @@ def geraDados(numeroDeProdutos):
 
         w.writerow([id, mes_1, mes_2, mes_3, mes_4, mes_5, mes_6, total, classificacao, mediaDia, estoqueIdeal])
     file.close()
-    #restart_program()
+    # restart_program()
 
 
-def csvParaLista():
+def csv_para_lista():
     file = open('../src/estoque.csv', 'r')
     reader = csv.reader(file)
     lista = []
@@ -67,44 +67,23 @@ def csvParaLista():
     file.close()
     return lista
 
-#Gerar grafico de proporção de produtos tipo A, B e C.
-def grafico2():
-    grupos = ["A", "B", "C"]
-    A = 0
-    B = 0
-    C = 0
 
-    lista = csvParaLista()
-
-    for i in range(1, len(lista)):
-        if(lista[i][8] == "A"):
-            A += 1
-        elif (lista[i][8] == "B"):
-            B += 1
-        else:
-            C += 1
-    valores = [A, B, C]
-    #print(valores)
-    plt.bar(grupos, valores)
-    plt.xlabel("Classificação")
-    plt.ylabel("Proporção")
-    plt.show()
-
-#Funções:
-def geraNovosDados(numeroDeProdutos, my_tree):
-    geraDados(numeroDeProdutos)
-    lista = csvParaLista()
+# Funções:
+def gera_novos_dados(numerodeprodutos, my_tree):
+    gera_dados(numerodeprodutos)
+    lista = csv_para_lista()
 
     # Adicionando intens na Tree.
-    for i in range(1, numeroDeProdutos):
+    for i in range(1, numerodeprodutos):
         listaAux = []
         for j in range(1, len(lista[i])):
             listaAux.append(lista[i][j])
         my_tree.item(i, text=lista[i][0], values=listaAux)
 
-#Gerar grafico de Tradicional x MC
-def grafico1():
-    lista = csvParaLista()
+
+# Gerar grafico de Tradicional x MC
+def grafico_1():
+    lista = csv_para_lista()
 
     mes_1 = 0
     mes_2 = 0
@@ -112,7 +91,7 @@ def grafico1():
     mes_4 = 0
     mes_5 = 0
     mes_6 = 0
-    meses =  [0,30,60,90,120,150]
+    meses = [0, 30, 60, 90, 120, 150]
     distNormal = [mes_1, mes_2, mes_3, mes_4, mes_5, mes_6]
     distMC = []
     somaMC = 0
@@ -123,7 +102,7 @@ def grafico1():
         distNormal[3] += int(lista[i][4])
         distNormal[4] += int(lista[i][5])
         distNormal[5] += int(lista[i][6])
-        somaMC += (float)(lista[i][10])
+        somaMC += float(lista[i][10])
 
     for i in range(0, len(meses)):
         distMC.append(somaMC)
@@ -135,8 +114,9 @@ def grafico1():
     plt.title("Tradicional x MC")
     plt.show()
 
-def grafico1_2():
-    lista = csvParaLista()
+
+def grafico_1_2():
+    lista = csv_para_lista()
     dias = []
     dia = 0
     produtosPorDia = []
@@ -152,7 +132,7 @@ def grafico1_2():
         mediaMensal.append(mediaDeProdutos)
 
     for i in range(0, NUMERODEMESES):
-        mediaDeProdutos = mediaMensal[i]/30
+        mediaDeProdutos = mediaMensal[i] / 30
         numeroDeProdutosDia = mediaMensal[i]
         for j in range(0, 30):
             dias.append(dia)
@@ -160,27 +140,59 @@ def grafico1_2():
             produtosPorDia.append(numeroDeProdutosDia)
             numeroDeProdutosDia -= mediaDeProdutos
 
-    for i in range(1, numeroDeProdutos+1):
+    for i in range(1, NUMERODEPRODUTOS + 1):
         mediaDiaTotal += float(lista[i][9])
         estoqueIdealTotal += float(lista[i][10])
 
     numeroEstoqueAtual = estoqueIdealTotal
-    for i in range(0, (NUMERODEMESES*30)):
+    for i in range(0, (NUMERODEMESES * 30)):
         valorAdicional = 0
-        if(i == 0):
+        if i == 0:
             numeroDeProdutosNoEstoque.append(numeroEstoqueAtual)
         else:
-            pass
+            if i % 8 == 0:
+                for j in range(1, NUMERODEPRODUTOS):
+                    if lista[j][8] == "C":
+                        valorAdicional += float(lista[j][10])
+            if i % 18 == 0:
+                for j in range(1, NUMERODEPRODUTOS):
+                    if lista[j][8] == "B":
+                        valorAdicional += float(lista[j][10])
+            if i % 28 == 0:
+                for j in range(1, NUMERODEPRODUTOS):
+                    if lista[j][8] == "A":
+                        valorAdicional += float(lista[j][10])
+            numeroEstoqueAtual = numeroEstoqueAtual - mediaDiaTotal + valorAdicional
             numeroDeProdutosNoEstoque.append(numeroEstoqueAtual)
-    #print(numeroDeProdutosNoEstoque)
-    #print(len(numeroDeProdutosNoEstoque))
-    #print(produtosPorDia)
 
     plt.plot(dias, produtosPorDia, "r")
     plt.plot(dias, numeroDeProdutosNoEstoque, "blue")
     plt.show()
 
-cont = ["#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17", "#18", "#19", "#20"]
-#print(csvParaLista()[0])
 
-#grafico1_2()
+# Gerar grafico de proporção de produtos tipo A, B e C.
+def grafico_2():
+    grupos = ["A", "B", "C"]
+    A = 0
+    B = 0
+    C = 0
+
+    lista = csv_para_lista()
+
+    for i in range(1, len(lista)):
+        if lista[i][8] == "A":
+            A += 1
+        elif lista[i][8] == "B":
+            B += 1
+        else:
+            C += 1
+    valores = [A, B, C]
+    # print(valores)
+    plt.bar(grupos, valores)
+    plt.xlabel("Classificação")
+    plt.ylabel("Proporção")
+    plt.show()
+
+
+CONTADORES = ["#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16",
+        "#17", "#18", "#19", "#20"]
--- src/app.py
+++ src/app.py
@@ -1,32 +1,34 @@
 from lib.gestorDeDados import *
+import tkinter as tk
+from tkinter import ttk
 
-#Janela principal
+# Janela principal
 janela = tk.Tk()
-largura = 625
-altura = 300
+LARGURA = 625
+ALTURA = 300
 janela.title("Controle de Estoque")
 janela.resizable(0, 0)
-janela.geometry("%dx%d" % (largura, altura))
+janela.geometry("%dx%d" % (LARGURA, ALTURA))
 janela.config(bg="#ADD8E6")
 
-lista = csvParaLista()
+lista = csv_para_lista()
 
-#tabela.
+# tabela.
 my_tree = ttk.Treeview(janela)
 my_tree['columns'] = (lista[0])
 
-#Passando os dados da lista para a Tree.
+# Passando os dados da lista para a Tree.
 for i in range(0, len(lista[0])):
-    if (i > 0 and i < 7):
-        my_tree.column(cont[i], width=40, minwidth=25)
-        my_tree.heading(cont[i], text=lista[0][i], anchor=tk.W)
+    if 0 < i < 7:
+        my_tree.column(CONTADORES[i], width=40, minwidth=25)
+        my_tree.heading(CONTADORES[i], text=lista[0][i], anchor=tk.W)
 
     else:
-        if(i>11):
+        if i > 11:
             break
-        my_tree.column(cont[i], width=77, minwidth=25)
-        my_tree.heading(cont[i], text=lista[0][i], anchor=tk.N)
-#Adicionando intens na Tree.
+        my_tree.column(CONTADORES[i], width=77, minwidth=25)
+        my_tree.heading(CONTADORES[i], text=lista[0][i], anchor=tk.N)
+# Adicionando intens na Tree.
 for i in range(1, len(lista)):
     listaAux = []
     for j in range(1, len(lista[i])):
@@ -34,14 +36,17 @@ for i in range(1, len(lista)):
     my_tree.insert(parent="", index='end', iid=i, text=lista[i][0], values=listaAux)
 my_tree.pack(pady=20)
 
-#my_tree.item(1, values="0")
+# my_tree.item(1, values="0")
 
-#Container para armazenar os botões.
+# Container para armazenar os botões.
 container = tk.Frame(master=janela, bg="white", pady=2).pack()
 
-#Botões.
-botao1 = tk.Button(master=container, text="Grafico 2", width=10, height=15, command=lambda: grafico2()).pack(side=tk.RIGHT)
-botao2 = tk.Button(master=container, text="Grafico 1", width=10, height=15, command=lambda: grafico1_2()).pack(side=tk.RIGHT, padx=2)
-botao3 = tk.Button(master=container, text="Gerar", width=10, height=15, command=lambda: geraNovosDados(numeroDeProdutos, my_tree)).pack(side=tk.RIGHT)
+# Botões.
+botao1 = tk.Button(master=container, text="Grafico 2", width=10, height=15, command=lambda: grafico_2()).pack(
+    side=tk.RIGHT)
+botao2 = tk.Button(master=container, text="Grafico 1", width=10, height=15, command=lambda: grafico_1_2()).pack(
+    side=tk.RIGHT, padx=2)
+botao3 = tk.Button(master=container, text="Gerar", width=10, height=15,
+                   command=lambda: gera_novos_dados(NUMERODEPRODUTOS, my_tree)).pack(side=tk.RIGHT)
 
 tk.mainloop()
